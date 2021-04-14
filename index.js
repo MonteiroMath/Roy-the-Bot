f612edd7cee24be46b366cbe33395482d575d575
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-04-14 07:59:45 -0300

    filter by channel and bot doesn't mention users anymore

--- index.js
+++ index.js
@@ -6,21 +6,23 @@ require("dotenv").config();
 const TOKEN = process.env.BOT_TOKEN;
 const CLIENT = new Discord.Client();
 const PREFIX = "!roy";
+const CHANNEL = "foro";
 
 CLIENT.on("message", function (message) {
   if (message.author.bot) return;
+  if (message.channel.name !== CHANNEL) return;
   if (!message.content.startsWith(PREFIX)) return;
 
   let { command, args } = parseCommand(message.content);
 
-  if (!command) return message.reply(COMMANDS.help());
+  if (!command) return message.channel.send(COMMANDS.help());
 
   if (!COMMANDS.hasOwnProperty(command))
-    return message.reply("Nao sei o que fazer com esse comando");
+    return message.channel.send("Nao sei o que fazer com esse comando");
 
   let response = COMMANDS[command](args);
 
-  message.reply(response);
+  message.channel.send(response);
 });
 
 CLIENT.login(TOKEN);
