c3957beeb791230a59244cdb8da854ad71980d8e
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-04-14 08:29:14 -0300

    clean help

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,6 @@ function help() {
   const HELP = `
   Comandos:
   oi: cumprimenta o Roy,
-  test: faz um test sem argumentos,
-  testWithArgs: faz um test com argumentos,
   bandeto: ban no deto,
   desbandeto: desban no deto
 `;
