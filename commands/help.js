a8760155dbf678eb3a01af811212d516f1b809e8
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-04-20 09:56:53 -0300

    help update

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,12 @@
 function help() {
   const HELP = `
   Comandos:
-  oi: cumprimenta o Roy,
+  oi: inicia um diálogo altamente realista com o Roy,
   bandeto: ban no deto,
   desbandeto: desban no deto,
-  post [main | autor ]: post aleatório do foro.
+  post [main | autor]: post aleatório do foro.
   classic [ autor ]: post aleatório do chat clássico.
+  quote: quota um post ou recupera um post quotado
 `;
 
   return HELP;
