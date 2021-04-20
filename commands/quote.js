7a1611598d7cc29c81fce0cb56d58a975ff74a48
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-04-20 09:19:44 -0300

    function extractions on quote command

--- commands/quote.js
+++ commands/quote.js
@@ -2,33 +2,42 @@ const dbAdapter = require("../helpers/dbAdapter");
 const formatMessage = require("../helpers/formatMessage");
 const mysql = require("mysql");
 
-function quote(args, ref) {
-  let query;
+const DB = "quotes";
 
-  if (!ref) {
-    //todo fetch a quote and return
-
-    query = `
+function getQuote() {
+  let query = `
     SELECT author, content, time 
     FROM quotes
     ORDER BY RAND()
     LIMIT 1;
   `;
 
-    return dbAdapter
-      .getData("quotes", query)
-      .then((result) => formatMessage(result[0]))
-      .catch((err) => {
-        console.log(err);
-        return "deu algum pau no sistema eu acho";
-      });
+  return dbAdapter
+    .getData(DB, query)
+    .then((result) => formatMessage(result[0]))
+    .catch((err) => {
+      console.log(err);
+      return "deu algum pau no sistema eu acho";
+    });
+}
+
+function insertQuote(ref) {
+  let content = mysql.escape(ref.content);
+
+  let query = `
+  INSERT INTO quotes (channel, message, author, content, time)
+  VALUES ${(ref.channel, ref.message, ref.author, content, ref.time)};
+  `;
+
+  return;
+}
+
+function quote(args, ref) {
+  if (!ref) {
+    return getQuote;
   }
 
-  // query = `
-  // INSERT INTO quotes (channel, message, author, content, time)
-  // VALUES ${ref.channel, ref.message, ref.author, ref.content, ref.time};
-  // `
-  //todo save quote to db
+  return insertQuote(ref);
 }
 
 module.exports = quote;
